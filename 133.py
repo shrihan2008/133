f26c386fb76472c874c08c6e50c4fb621813db43
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-07 17:59:32 +0530

    Add files via upload

--- 133.py
+++ 133.py
@@ -32,26 +32,6 @@ for index,name in enumerate(star_name):
 
 
 
-name=[]
-import csv
-with open("dwarf_stars.csv",'r') as f:
-  f1=csv.reader(f)
-  for a  in f1:
-    name.append(a)
-star_data=name[1:]
-gravity=[]
-mass=[]
-radius=[]
-name=[]
-for i in star_data:
-  mass.append(i[3])
-  radius.apeend(i[4])
-  name.append(i[1])
-
-for index,name in enumerate(name):
-  g=(float(mass[index])) *6.17/100000000000/(float(radius[index]) * float(radius[index]) ) 
-  gravity.append(g) 
-
 low_gravity_stars=[]
 
 for index,gravity in enumerate(star_gravity):
